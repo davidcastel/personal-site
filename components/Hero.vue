86b7b9121c2aba2f90243c6d9937e6220a023a42
davidcastel <davidccbp20@gmail.com>
2021-06-25 21:28:45 -0400

    Create Hero component

---
+++ components/Hero.vue
@@ -0,0 +1,34 @@
+<template>
+  <!-- Hero -->
+  <v-container fluid tag="section">
+    <v-row>
+      <v-card width="100vw">
+        <v-img
+          height="100vh"
+          :src="require(`@/assets/images/Sky Line.jpg`)"
+          class="align-center"
+        >
+          <v-card-title height="100vh" class="d-flex flex-column">
+            <h1>Hello, I am :</h1>
+            <h3>David, nice to meet you</h3>
+          </v-card-title>
+        </v-img>
+      </v-card>
+    </v-row>
+  </v-container>
+  <!-- End of Hero section -->
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.hero-title {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 100%;
+}
+</style>
