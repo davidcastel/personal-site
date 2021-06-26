68bdf700ee57872abbfe8fc06c5ca37600d1e4e0
davidcastel <davidccbp20@gmail.com>
2021-06-25 21:27:23 -0400

    Fix object naming

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: "personal-site",
-    htmlAttr: {
+    htmlAttrs: {
       lang: "en"
     },
     meta: [
@@ -30,7 +30,7 @@ export default {
           "https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
       }
     ],
-    scripts: [
+    script: [
       // Jquery
       {
         src: "https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"
