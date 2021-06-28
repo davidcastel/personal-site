5c4a89259cc337ef0121113ec9a9169798866760
davidcastel <davidccbp20@gmail.com>
2021-06-28 00:08:37 -0400

    Create data and loopable components for desgin concept

--- components/SkillsCard.vue
+++ components/SkillsCard.vue
@@ -1,54 +1,74 @@
 <template>
   <!-- Skils -->
-  <div
-    class="tab-pane fade"
-    id="skills-list"
-    role="tabpanel"
-    aria-labelledby="lists-tab"
-  >
-    <!-- Bilingual -->
-    <div class="row">
-      <div class="col-5 col-sm-3 col-md-2">
-        <h4>Bilingual :</h4>
-      </div>
-      <div class="col">
-        <p>Spanish</p>
-      </div>
-    </div>
-    <!-- Languages -->
-    <div class="row">
-      <div class="col-5 col-sm-3 col-md-2">
-        <h4>Languages :</h4>
-      </div>
-      <div class="col">
-        <p>HTML/CSS, Python, Java, JavaScript</p>
-      </div>
-    </div>
-    <!-- Frameworks -->
-    <div class="row">
-      <div class="col-5 col-sm-3 col-md-2">
-        <h4>Frameworks :</h4>
-      </div>
-      <div class="col">
-        <p>Bootstrap</p>
-      </div>
-    </div>
-    <!-- Tools -->
-    <div class="row">
-      <div class="col-5 col-sm-3 col-md-2">
-        <h4>Tools :</h4>
-      </div>
-      <div class="col">
-        <p>
-          Command-line, Git, Jekyll, VsCode, Vim, Slack, Asana, Figma, Github
-        </p>
-      </div>
-    </div>
-  </div>
+  <v-container class="tab-pane fade" id="skills-list">
+    <v-row v-for="skill in skills" :key="skill" class="card">
+      <v-col cols="12" sm="4" md="3">
+        <h4 class="text-center text-sm-right">{{ skill.title }} :</h4>
+      </v-col>
+      <v-col cols="12" sm="8">
+        <template v-if="skill.isRows">
+          <p class="mb-0">
+            <template v-for="item in skill.skill">
+              {{ item + " " }}
+            </template>
+          </p>
+        </template>
+        <template v-else>
+          <v-row v-for="item in skill.skill" :key="item">
+            <p>
+              {{ item.title }}
+            </p>
+            <v-progress-linear
+              height="10"
+              :color="item.color"
+              :value="item.progress"
+              width="80"
+              striped
+              rounded
+            ></v-progress-linear>
+          </v-row>
+        </template>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      skills: [
+        { title: "Bilingual", isRows: true, skill: ["Spanish"] },
+        {
+          title: "Languages",
+          isRows: false,
+          skill: [
+            { title: "HTML/CSS", progress: 90, color: "red" },
+            { title: "JavaScript", progress: 80, color: "orange" },
+            { title: "Python", progress: 75, color: "light-blue" },
+            { title: "Java", progress: 45, color: "green" },
+          ],
+        },
+        { title: "Frameworks", isRows: true, skill: ["Bootstrap"] },
+        {
+          title: "Tools",
+          isRows: true,
+          skill: [
+            "Command-line,",
+            "Git,",
+            "Jekyll,",
+            "VsCode,",
+            "Vim,",
+            "Slack,",
+            "Asana,",
+            "Figma,",
+            "Github,",
+          ],
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
