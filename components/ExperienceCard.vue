0df76b0665c972cd12a900ad8a673e509f3f0070
davidcastel <davidccbp20@gmail.com>
2021-06-28 00:09:08 -0400

    Create Expansion panels for each experience

--- components/ExperienceCard.vue
+++ components/ExperienceCard.vue
@@ -1,76 +1,55 @@
 <template>
   <!-- Experience -->
-  <div
+  <v-expansion-panels
     class="tab-pane fade"
     id="experience-list"
     role="tabpanel"
     aria-labelledby="experience-tab"
   >
-    <div class="item w-50">
-      <h4 class="">
-        <a
-          class=""
-          data-bs-toggle="collapse"
-          href="#collapse1"
-          role="button"
-          aria-expanded="false"
-          aria-controls="collapse1"
-        >
-          Participant <br />
-          <span class="small">Google - Software Product Sprint</span>
-          <span class="float-end small">2021 - Present</span>
-        </a>
-      </h4>
-      <div class="collapse" id="collapse1">
-        <div class="card card-body"></div>
-      </div>
-    </div>
-    <div class="item w-50 ms-auto">
-      <h4 class="">
-        <a
-          class=""
-          data-bs-toggle="collapse"
-          href="#collapse2"
-          role="button"
-          aria-expanded="false"
-          aria-controls="collapse2"
-        >
-          Junior Developer<br />
-          <span class="small">Draft Studios</span>
-          <span class="float-end small">2019 - Present</span>
-        </a>
-      </h4>
-      <div class="collapse" id="collapse2">
-        <div class="card card-body"></div>
-      </div>
-    </div>
-    <div class="item w-50">
-      <h4 class="">
-        <a
-          class=""
-          data-bs-toggle="collapse"
-          href="#collapse3"
-          role="button"
-          aria-expanded="false"
-          aria-controls="collapse3"
-        >
-          Participant<br />
-          <span class="small"
-            >Google - Computer Science Summer Institute (CSSI)</span
-          >
-          <span class="float-end small">2019</span>
-        </a>
-      </h4>
-      <div class="collapse" id="collapse3">
-        <div class="card card-body"></div>
-      </div>
-    </div>
-  </div>
+    <v-expansion-panel v-for="project in projects" :key="project">
+      <v-expansion-panel-header>
+        <h4>
+          {{ project.title }}
+          <br />
+          <span>{{ project.company }}</span> <br />
+          <span>{{ project.timeFrame }}</span>
+        </h4>
+      </v-expansion-panel-header>
+      <v-expansion-panel-content>
+        {{ project.body }}
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+  </v-expansion-panels>
   <!-- End of Experience -->
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      projects: [
+        {
+          title: "Participant",
+          company: "Google - Software Product Sprint",
+          timeFrame: "2021 - Present",
+          body: "",
+        },
+        {
+          title: "Junior Developer",
+          company: "Draft Studios",
+          timeFrame: "2019 - Present",
+          body: "",
+        },
+        {
+          title: "Participant",
+          company: "Google - Computer Science Summer Institute (CSSI)",
+          timeFrame: "2019",
+          body: "",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
