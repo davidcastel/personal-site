26c3fbcaa09aaf8ac6bd209afffea31aa965dc6c
davidcastel <davidccbp20@gmail.com>
2021-07-12 00:12:00 -0400

    Set up ProjectSection component

---
+++ components/ProjectSection.vue
@@ -0,0 +1,110 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col></v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import ProjectCard from "./ProjectCard.vue";
+export default {
+  components: {
+    ProjectCard,
+  },
+  data: {
+    projects: [
+      {
+        id: "maze-project",
+        cardItems: {
+          title: "",
+          img: "",
+          body: "",
+          languages: [],
+          btn_repo: "See Source Code",
+          repo_link: "",
+          hasLink: false,
+          btn_text: "",
+          btn_link: "",
+        },
+      },
+      {
+        id: "banking-project",
+        cardItems: {
+          title: "",
+          img: "",
+          body: "",
+          languages: [],
+          btn_repo: "See Source Code",
+          repo_link: "",
+          hasLink: false,
+          btn_text: "",
+          btn_link: "",
+        },
+      },
+      {
+        id: "path_of_hope-project",
+        cardItems: {
+          title: "",
+          img: "",
+          body: "",
+          languages: [],
+          btn_repo: "See Source Code",
+          repo_link: "",
+          hasLink: false,
+          btn_text: "",
+          btn_link: "",
+        },
+      },
+      {
+        id: "wheell-project",
+        cardItems: {
+          title: "",
+          img: "",
+          body: "",
+          languages: [],
+          btn_repo: "See Source Code",
+          repo_link: "",
+          hasLink: false,
+          btn_text: "",
+          btn_link: "",
+        },
+      },
+      {
+        id: "CVS_library-project",
+        cardItems: {
+          title: "",
+          img: "",
+          body: "",
+          languages: [],
+          btn_repo: "See Source Code",
+          repo_link: "",
+          hasLink: false,
+          btn_text: "",
+          btn_link: "",
+        },
+      },
+      {
+        id: "calculator-project",
+        cardItems: {
+          title: "",
+          img: "",
+          body: "",
+          languages: [],
+          btn_repo: "See Source Code",
+          repo_link: "",
+          hasLink: false,
+          btn_text: "",
+          btn_link: "",
+        },
+      },
+    ],
+  },
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
